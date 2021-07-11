e4e8ac611e0b73f15a4407f196e0e2545ac91432
yazilimfuryasi <84662757+yazilimfuryasi@users.noreply.github.com>
2021-07-11 23:54:46 +0300

    Add files via upload
    imdb.com top 250 list.

---
+++ imdb top 250.py
@@ -0,0 +1,17 @@
+# instagram.com/yazilimfuryasi
+# yazilimfuryasi.com
+
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.imdb.com/chart/top/?ref_=nv_mv_250"
+r = requests.get(url).content
+soup = BeautifulSoup(r, "html.parser") 
+list = soup.find("tbody", {"class":"lister-list"}).find_all("tr",limit=250)
+s = 1
+for i in list:
+    title = i.find("td",{"class":"titleColumn"}).find("a").text
+    yil = i.find("span",{"class":"secondaryInfo"}).text.strip("()")
+    rating = i.find("td",{"class":"ratingColumn"}).find("strong").text
+    print(f"{s}. {title.ljust(50)} {yil}   Rating: {rating}")
+    s += 1
